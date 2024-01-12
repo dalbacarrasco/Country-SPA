b7dcbc6bb7f00c2411e8c7e23cbdab8f7418a403
Daniela Carrasco <90220457+dalbacarrasco@users.noreply.github.com>
2024-01-12 17:55:09 -0400

    routerLink y routerLinkOptions

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { RouterModule } from '@angular/router';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 
 
 
@@ -10,13 +13,18 @@ import { AboutPageComponent } from './pages/about-page/about-page.component';
   declarations: [
     HomePageComponent,
     AboutPageComponent,
+    SidebarComponent,
+    ContactPageComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
   ],
   exports: [
     HomePageComponent,
     AboutPageComponent,
+    SidebarComponent,
+    ContactPageComponent
   ]
 })
 export class SharedModule { }
